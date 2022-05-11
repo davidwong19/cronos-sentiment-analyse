365a33897a150024a8fa30bcec1736b90f3a7c63
Rehtsecp <rehtseput@gmail.com>
2022-05-11 10:25:24 +0200

    Changes in automation.bat

--- Automation/gd_auto.bat
+++ Automation/gd_auto.bat
@@ -1,3 +1,3 @@
-"C:\Users\Rehts\AppData\Local\Programs\Python\Python39\python.exe" REM Path where your Python exe is stored\python.exe
+"C:\Users\Rehts\AppData\Local\Programs\Python\Python39\python.exe" REM Path where your Python exe is stored python.exe
 "..\Scrapers\glassdoor_reviews\gd_scraper.py"
 pause
