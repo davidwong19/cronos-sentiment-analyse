cbec90e16349770cff90d34f88615ba8ce69d1c3
Rehtsecp <rehtseput@gmail.com>
2022-05-11 11:45:00 +0200

    Changes in indeed_scraper.py

--- Automation/gd_auto.bat
+++ Automation/gd_auto.bat
@@ -1,3 +1,3 @@
-:: REM Path where your python.exe is stored                         REM Does not need to be changed
+:: REM Path where your python.exe is stored                         REM REM Path to gd_scraper.py
 "C:\Users\Rehts\AppData\Local\Programs\Python\Python39\python.exe" "..\Scrapers\glassdoor_reviews\gd_scraper.py"
 pause
