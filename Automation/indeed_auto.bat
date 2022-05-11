f6148502f3022f26269caefa4b8ecc441318437f
Rehtsecp <rehtseput@gmail.com>
2022-05-11 11:42:59 +0200

    Added indeed_auto.bat

---
+++ Automation/indeed_auto.bat
@@ -0,0 +1,3 @@
+:: REM Path where your python.exe is stored                         REM Does not need to be changed
+"C:\Users\Rehts\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\Rehts\Documents\Repositories\cronos-sentiment-analyse\Scrapers\indeed_reviews\indeed_scraper.py"
+pause
