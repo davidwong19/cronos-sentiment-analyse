5ffe8ce2c2db18638d15ad1e5e33b9258c70d47a
Rehtsecp <rehtseput@gmail.com>
2022-05-11 11:45:20 +0200

    Changes in indeed_scraper.py

--- Automation/indeed_auto.bat
+++ Automation/indeed_auto.bat
@@ -1,3 +1,3 @@
-:: REM Path where your python.exe is stored                         REM Does not need to be changed
+:: REM Path where your python.exe is stored                         REM Path to indeed_scraper.py
 "C:\Users\Rehts\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\Rehts\Documents\Repositories\cronos-sentiment-analyse\Scrapers\indeed_reviews\indeed_scraper.py"
 pause
