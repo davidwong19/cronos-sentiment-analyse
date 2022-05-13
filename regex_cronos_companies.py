8fda863636a5a57a56e4f82f366acd7fe8575616
Rehtsecp <rehtseput@gmail.com>
2022-05-13 15:24:49 +0200

    Too many changes

---
+++ regex_cronos_companies.py
@@ -0,0 +1,20 @@
+from cmath import e
+import re
+
+file = open('staatsbladmonitor.txt', 'r')
+
+list_companies = []
+
+for line in file:
+    line = line.strip()
+    r = re.findall(r'(label: \'\w+\')|(label: \'\w+ \w+\')|(label: \'\w+ \w+ \w+\')|(label: \'\w+ \w+ \w+ \w+\')', line)
+    string_r = ''.join(map(str,r)).strip()
+
+    labels = re.findall(r'(?s)(?<=label: \').*?(?=\'\")', str(string_r))
+    string_labels = ''.join(map(str,labels))
+    
+    list_companies.append(string_labels)
+
+with open('companies_staatsblad.txt', 'w') as f:
+    for item in list_companies:
+        f.write(item)
